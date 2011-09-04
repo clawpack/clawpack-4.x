ecd76786567485ffab2d6e77ac516cfec106d0d6
Randy LeVeque <rjl@uw.edu>
2011-09-04 00:18:57 -0700

    Modifications to plotfg module needed for NTHMP Okushiri benchmark.
    Added some attributes.  Need to clean up further for general use.

--- python/pyclaw/plotters/plotfg.py
+++ python/pyclaw/plotters/plotfg.py
@@ -29,23 +29,49 @@ class ClawPlotFGData(Data):
         self.add_attribute('drytol',1.e-2)
         self.add_attribute('exposed_tol',1.e-2)
         self.add_attribute('clines',linspace(-20,20,21))
+
         self.add_attribute('water_cmap', geoplot.tsunami_colormap)
         self.add_attribute('land_cmap', geoplot.land_colors)
         self.add_attribute('seafloor_cmap', geoplot.seafloor_colormap)
         self.add_attribute('inundated_cmap', \
              colormaps.make_colormap({0.:[1,.9,.9], 1.:[1,0,0]}))
+
         self.add_attribute('water_clim',(-1,1))
         self.add_attribute('land_clim',(0,10))
         self.add_attribute('seafloor_clim',(-1,0))
         self.add_attribute('inundated_clim',(0,1))
+
+        self.add_attribute('water_add_colorbar',True)
+        self.add_attribute('land_add_colorbar',False)
+        self.add_attribute('seafloor_add_colorbar',True)
+        self.add_attribute('inundated_add_colorbar',True)
+
         self.add_attribute('eta_show',True)
         self.add_attribute('seafloor_show',True)
         self.add_attribute('inundated_show',True)
+
         self.add_attribute('outdir','_output')
         self.add_attribute('plotdir',None)
         self.add_attribute('save_png',False)
         self.add_attribute('solutions',{})
         self.add_attribute('grids',{})
+
+    def list_frames(self):
+
+        import glob
+        pattern = "%s/fort.fg%s_*" % (self.outdir,str(self.fgno).zfill(2))
+        files = glob.glob(pattern)
+        if len(files) == 0:
+            print '*** No files found of form ', pattern
+        framenos = []
+        for file in files:
+            line = open(file,'r').readline()
+            t = float(line.split()[0])
+            print "%s: t = %s" % (file,t)
+            frameno = file[-2:]
+            framenos.append(int(frameno))
+        return framenos
+
 
     def get_frame(self, frameno):
 
@@ -128,6 +154,9 @@ class ClawPlotFGData(Data):
             contour(grid.xcenter,grid.ycenter,solution.B,self.clines,colors='k')
             contour(grid.xcenter,grid.ycenter,solution.B,[0.],colors='k',linewidths=2)
     
+        #t_str = "%10.3f" % solution.t
+        tmin = solution.t / 60.
+        t_str = "%8.2f minutes" % tmin
     
         figno = 150
         figure(figno)
@@ -136,20 +165,22 @@ class ClawPlotFGData(Data):
         if ma.count(solution.surface) != 0:
             pcolormesh(grid.x,grid.y,solution.surface,cmap=self.water_cmap)
             clim(self.water_clim)
-            colorbar()
+            if self.water_add_colorbar: colorbar()
         
         if ma.count(solution.land) != 0:
             pcolormesh(grid.x,grid.y,solution.land,cmap=self.land_cmap)
-            clim(self.water_clim)
+            clim(self.land_clim)
+            if self.land_add_colorbar: colorbar()
         
         add_contours()
         
         
-        axis('scaled')
-        title('Eta on FG %s at time = %10.3f' % (self.fgno,solution.t))
+        title('Eta on FG %s at time = %s' % (self.fgno,t_str))
         xlim((grid.xlow, grid.xhi))
         ylim((grid.ylow, grid.yhi,))
         fix_long_tick_labels()        
+        axis('tight')
+        axis('scaled')
     
         if self.save_png:
             fname = 'FixedGrid%sFrame%sfig%s.png' \
@@ -177,7 +208,9 @@ class ClawPlotFGData(Data):
             exposed_tol = self.exposed_tol
             drytol = self.drytol
             exposed = ma.masked_where(((B>0) | (etamin > B+exposed_tol)), etamin)
-            inundated = ma.masked_where(((B<0) | (etamax < B+drytol)), etamax)
+            inundated = ma.masked_where(((B<0) | (etamax < B+drytol)), etamax-B)
+
+        #---------------------------------------------------------------
 
         if self.inundated_show:
             if solution.ncols < 7:
@@ -190,21 +223,24 @@ class ClawPlotFGData(Data):
             if ma.count(inundated) != 0:
                 pcolormesh(grid.x,grid.y,inundated,cmap=self.inundated_cmap)
                 clim(self.inundated_clim)
-                colorbar()
+                if self.inundated_add_colorbar: colorbar()
             add_contours()
             # Add red contour of maximum eta
             #contour(xcenter,ycenter,etamax2,[drytol],colors='r',linewidths=2)
-            title("Inundated region for t <= %10.3f" % solution.t)
-            axis('scaled')
+            title("Inundated region for t <= %s" % t_str)
             xlim((grid.xlow, grid.xhi))
             ylim((grid.ylow, grid.yhi,))
             fix_long_tick_labels()        
+            axis('tight')
+            axis('scaled')
             if self.save_png:
                 fname = 'FixedGrid%sFrame%sFig%s.png' \
                     %  (str(self.fgno).zfill(2), str(frameno).zfill(4), figno)
                 savefig(fname)
                 print "Saved figure as ",fname
         
+        #---------------------------------------------------------------
+
         if self.seafloor_show:
             if solution.ncols < 7:
                 raise ValueError("*** Data does not include etamin/etamax")
@@ -216,15 +252,16 @@ class ClawPlotFGData(Data):
             if ma.count(exposed) != 0:
                 pcolormesh(grid.x,grid.y,exposed,cmap=self.seafloor_cmap)
                 clim(self.seafloor_clim)
-                colorbar()
+                if self.exposed_add_colorbar: colorbar()
             add_contours()
             # Add brown contour of minimum eta
             #contour(xcenter,ycenter,etamin-B,[exposed_tol],colors=([.9,.8,.2],),linewidths=2)
-            title("Exposed seabed for t <= %10.3f" % solution.t)
-            axis('scaled')
+            title("Exposed seabed for t <= %s" % t_str)
             xlim((grid.xlow, grid.xhi))
             ylim((grid.ylow, grid.yhi,))
             fix_long_tick_labels()        
+            axis('tight')
+            axis('scaled')
             if self.save_png:
                 fname = 'FixedGrid%sFrame%sFig%s.png' \
                     %  (str(self.fgno).zfill(2), str(frameno).zfill(4), figno)
@@ -286,6 +323,7 @@ class ClawPlotFGData(Data):
         ppd.timeframes_fignames[152] = 'Exposed seafloor'
         plotpages.timeframes2html(ppd)
             
+
 
 if __name__ == "__main__":
     # if executed at the Unix command line....
