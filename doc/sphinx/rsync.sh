dd1d303abd9ad500ea185a0f4047947bdafacb48
Randy LeVeque <rjl@uw.edu>
2012-02-02 15:55:09 +0300

    Modified rsync to add example directories for documentation

--- doc/sphinx/rsync.sh
+++ doc/sphinx/rsync.sh
@@ -2,7 +2,13 @@
 # Use to sync up files on clawpack.org server:
 
 echo Will rsync with clawpack@homer...
-chmod -R og+rX users
+chmod -R og+rX users example-*
 rsync -avz --delete users/ \
   clawpack@homer.u.washington.edu:public_html/users-4.x
+rsync -avz example-acoustics-1d \
+  clawpack@homer.u.washington.edu:public_html/
+rsync -avz example-acoustics-2d \
+  clawpack@homer.u.washington.edu:public_html/
+rsync -avz example-acoustics-2d-amr \
+  clawpack@homer.u.washington.edu:public_html/
 
